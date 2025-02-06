8333c367729182b3b1e04e9f51247316639713f4
Klaas Schoute <hello@student-techlife.com>
2025-02-07 00:19:35 +0100

    Update Tailwind CSS to v4.x (#249)
    * Update Tailwind CSS to v4.x
    
    * Fix flowbite source path in app.css

--- laravel/resources/views/pages/home.blade.php
+++ laravel/resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app title="Home" keywords="home">
     <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <div class="py-8 px-4 mx-auto max-w-(--breakpoint-xl) text-center lg:py-16 lg:px-12">
             <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                 <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Flowbite is out! See what's new</span> 
                 <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
@@ -17,7 +17,7 @@
                     Watch video
                 </a>
             </div>
-            <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+            <div class="px-4 mx-auto text-center md:max-w-(--breakpoint-md) lg:max-w-(--breakpoint-lg) lg:px-36">
                 <span class="font-semibold text-gray-400 uppercase">FEATURED IN</span>
                 <div class="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
                     <a href="#" class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
