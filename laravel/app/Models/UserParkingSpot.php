e12bc315395136a170d9d879ab42438025d084c7
Klaas Schoute <hello@student-techlife.com>
2025-02-12 01:32:43 +0100

    Add UserParkingSpot model, migration, and factory for parking spot (#260)

--- laravel/app/Models/UserParkingSpot.php
+++ laravel/app/Models/UserParkingSpot.php
@@ -9,11 +9,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class ParkingSpot extends Model
+class UserParkingSpot extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserParkingSpotFactory> */
     use HasFactory, SoftDeletes;
 
-    protected $table = 'parking_spots';
+    protected $table = 'user_parking_spots';
 
     protected $guarded = ['id'];
 
