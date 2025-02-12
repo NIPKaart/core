76810915232930623787b75313538a65fe927fb5
Klaas Schoute <hello@student-techlife.com>
2025-02-12 16:38:47 +0100

    Add parking-related relationships to Country, Province, and User models (#265)

--- laravel/app/Models/Province.php
+++ laravel/app/Models/Province.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Province extends Model
 {
@@ -23,5 +24,29 @@ class Province extends Model
     public function country(): BelongsTo
     {
         return $this->belongsTo(Country::class);
+    }
+
+    /**
+     * Get parking spots in the province.
+     */
+    public function parkingSpots(): HasMany
+    {
+        return $this->hasMany(UserParkingSpot::class);
+    }
+
+    /**
+     * Get parking offstreets in the province.
+     */
+    public function parkingOffstreets(): HasMany
+    {
+        return $this->hasMany(ParkingOffstreet::class);
+    }
+
+    /**
+     * Get parking spots provided by municipalities in the province.
+     */
+    public function municipalParkingSpots(): HasMany
+    {
+        return $this->hasMany(MunicipalParkingSpot::class);
     }
 }
