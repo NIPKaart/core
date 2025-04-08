306c851fd2ff6748ab98461e3eefcf7476527f08
Klaas Schoute <klaas_schoute@hotmail.com>
2025-04-08 10:30:52 +0200

    Add nav-section(s) components for sidebar

---
+++ resources/js/components/nav/nav-section.tsx
@@ -0,0 +1,25 @@
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { NavGroup } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
+
+export function NavSection({ group }: { group: NavGroup }) {
+    const page = usePage();
+
+    return (
+        <SidebarGroup className="px-2 py-0">
+            <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
+            <SidebarMenu>
+                {group.items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton asChild isActive={page.url === item.href} tooltip={{ children: item.title }}>
+                            <Link href={item.href} prefetch target={item.target}>
+                                {item.icon && <item.icon />}
+                                <span>{item.title}</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    );
+}
