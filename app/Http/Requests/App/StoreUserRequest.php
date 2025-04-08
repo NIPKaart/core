5d4f0ed06395b5e52171d6e332e1381e309f9b49
Klaas Schoute <klaas_schoute@hotmail.com>
2025-04-09 01:56:33 +0200

    Add StoreUserRequest and UpdateUserRequest for user validation

---
+++ app/Http/Requests/App/StoreUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\App;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('user.create');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', 'min:8'],
+            'role' => ['required', 'exists:roles,name'],
+        ];
+    }
+}
