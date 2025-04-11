5faf57e16cdf43e78ddd3b56e9aee68f761bfe43
Klaas Schoute <klaas_schoute@hotmail.com>
2025-04-12 01:30:41 +0200

    Add 'suspended_at' field to UserFactory definition

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'suspended_at' => null,
         ];
     }
 
