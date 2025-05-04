8a256944f70cd2069a979ce3244daaf404d076d7
Klaas Schoute <klaas_schoute@hotmail.com>
2025-05-04 23:53:52 +0200

    Add ParkingOffstreet model, factory, and migration

---
+++ app/Models/ParkingOffstreet.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ParkingOffstreet extends Model
+{
+    /** @use HasFactory<\Database\Factories\ParkingOffstreetFactory> */
+    use HasFactory;
+
+    protected $table = 'parking_offstreets';
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'country_id',
+        'province_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'id' => 'string',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Get the country that owns the parking offstreet.
+     */
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    /**
+     * Get the province that owns the parking offstreet.
+     */
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+}
---
+++ database/migrations/2025_05_04_214643_create_parking_offstreets_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parking_offstreets', function (Blueprint $table) {
+            $table->string('id')->primary(); // External ID as primary key
+            $table->string('name');
+            $table->string('municipality');
+
+            $table->foreignId('country_id')->constrained('countries');
+            $table->foreignId('province_id')->constrained('provinces');
+
+            // Parking details
+            $table->integer('free_space_short');
+            $table->integer('free_space_long')->nullable();
+            $table->integer('short_capacity');
+            $table->integer('long_capacity')->nullable();
+            $table->double('availability_pct')->nullable();
+            $table->enum('parking_type', ['garage', 'parkandride']);
+            $table->json('prices')->nullable();
+            $table->string('url')->nullable();
+
+            // Parking location
+            $table->decimal('longitude', 10, 7);
+            $table->decimal('latitude', 10, 7);
+
+            $table->string('state')->nullable();
+            $table->boolean('visibility');
+            $table->timestamps();
+
+            // Indexes
+            $table->index('municipality');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parking_offstreets');
+    }
+};
