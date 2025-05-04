0aed9c934306f9d1c7cba67bfbdd7788030b411f
Klaas Schoute <klaas_schoute@hotmail.com>
2025-05-05 01:59:03 +0200

    Move Role and User controller to Admin folder

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\App\StoreRoleRequest;
 use App\Http\Requests\App\UpdateRoleRequest;
 use App\Models\Role;
