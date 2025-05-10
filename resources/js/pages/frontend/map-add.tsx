535ff8ceeb3234fdf832ca5b0e19b07a0d07da6a
Klaas Schoute <klaas_schoute@hotmail.com>
2025-05-10 14:47:17 +0200

    Add map component for location addition with draggable marker

---
+++ resources/js/pages/frontend/map-add.tsx
@@ -0,0 +1,55 @@
+import Navbar from '@/components/frontend/nav/nav-bar';
+import { Head } from '@inertiajs/react';
+import type { LatLngExpression } from 'leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useCallback, useState } from 'react';
+import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
+
+const orangeIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/images/marker-shadow.png',
+    iconSize: [33, 48],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
+
+function ClickHandler({ onClick }: { onClick: (latlng: LatLngExpression) => void }) {
+    useMapEvents({
+        click(e) {
+            onClick(e.latlng);
+        },
+    });
+    return null;
+}
+
+export default function AddLocation() {
+    const [markerPosition, setMarkerPosition] = useState<LatLngExpression | null>(null);
+
+    // Handle map click to set marker position
+    const handleMapClick = useCallback((latlng: LatLngExpression) => {
+        setMarkerPosition(latlng);
+    }, []);
+
+    // Handle marker drag end to update position
+    const handleDragEnd = useCallback((e: L.LeafletEvent) => {
+        const marker = e.target as L.Marker;
+        const position = marker.getLatLng();
+        setMarkerPosition([position.lat, position.lng]);
+    }, []);
+
+    return (
+        <>
+            <Head title="Add Location" />
+            <div className="flex h-[100dvh] flex-col">
+                <Navbar />
+                <MapContainer center={[52.3676, 4.9041]} zoom={15} scrollWheelZoom className="z-0 h-full w-full">
+                    <TileLayer attribution="&copy; OpenStreetMap contributors" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <ClickHandler onClick={handleMapClick} />
+                    {markerPosition && <Marker position={markerPosition} icon={orangeIcon} draggable eventHandlers={{ dragend: handleDragEnd }} />}
+                </MapContainer>
+            </div>
+        </>
+    );
+}
