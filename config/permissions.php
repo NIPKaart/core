a333d1c0b0181cdbc15b1842948b5e12bf7f21d8
Klaas Schoute <klaas_schoute@hotmail.com>
2025-05-13 02:49:07 +0200

    Add UserParkingSpotPolicy with authorization methods and update permissions configuration

--- config/permissions.php
+++ config/permissions.php
@@ -9,6 +9,7 @@ return [
                     'role' => ['view', 'view_any', 'create', 'update', 'delete'],
                     'user' => ['view', 'view_any', 'create', 'update', 'delete'],
                     'parking-rule' => ['view', 'view_any', 'create', 'update', 'delete'],
+                    'user-parking-spot' => ['view', 'view_any', 'create', 'update', 'delete', 'force-delete', 'restore'],
                 ],
             ],
         ],
