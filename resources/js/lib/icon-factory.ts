b77728287da045d4fd050691496343d21d36550f
Klaas Schoute <klaas_schoute@hotmail.com>
2025-05-20 03:33:03 +0200

    Refactor icon factory to improve clarity and organization of parking icons

--- resources/js/lib/icon-factory.ts
+++ resources/js/lib/icon-factory.ts
@@ -1,5 +1,8 @@
 import L, { Icon } from 'leaflet';
 
+/**
+ * Icon for invalid parking spots.
+ */
 export function getInvalidParkingIcon(): Icon {
     return L.icon({
         iconUrl: '/assets/images/boards/e6.jpg',
@@ -8,4 +11,69 @@ export function getInvalidParkingIcon(): Icon {
         popupAnchor: [1, -34],
         shadowSize: [41, 41],
     });
+}
+
+/**
+ * Icon for parking garage: green (sufficient space).
+ */
+export function getGarageGreenIcon(): Icon {
+    return L.icon({
+        iconUrl: '/assets/images/boards/e105-green.png',
+        iconSize: [30, 30],
+        iconAnchor: [15, 30],
+        popupAnchor: [0, -30],
+        shadowSize: [41, 41],
+    });
+}
+
+/**
+ * Icon for parking garage: orange (almost full).
+ */
+export function getGarageOrangeIcon(): Icon {
+    return L.icon({
+        iconUrl: '/assets/images/boards/v2/e105-orange.png',
+        iconSize: [30, 30],
+        iconAnchor: [15, 30],
+        popupAnchor: [0, -30],
+        shadowSize: [41, 41],
+    });
+}
+
+/**
+ * Icon for parking garage: red (full).
+ */
+export function getGarageRedIcon(): Icon {
+    return L.icon({
+        iconUrl: '/assets/images/boards/v2/e105-red.png',
+        iconSize: [30, 30],
+        iconAnchor: [15, 30],
+        popupAnchor: [0, -30],
+        shadowSize: [41, 41],
+    });
+}
+
+/**
+ * Icon for parking garage: no data / old / error.
+ */
+export function getGarageGreyIcon(): Icon {
+    return L.icon({
+        iconUrl: '/assets/images/boards/v2/e105-grey.png',
+        iconSize: [30, 30],
+        iconAnchor: [15, 30],
+        popupAnchor: [0, -30],
+        shadowSize: [41, 41],
+    });
+}
+
+/**
+ * Get the icon for a parking garage based on its status.
+ */
+export function getGarageStatusIcon(status: 'green' | 'orange' | 'red' | 'grey'): Icon {
+    switch (status) {
+        case 'green': return getGarageGreenIcon();
+        case 'orange': return getGarageOrangeIcon();
+        case 'red': return getGarageRedIcon();
+        case 'grey': return getGarageGreyIcon();
+        default: return getGarageGreyIcon();
+    }
 }
