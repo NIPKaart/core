08117c328cd115b5d89ca42778b6a883db7aad85
Klaas Schoute <klaas_schoute@hotmail.com>
2025-05-28 02:37:59 +0200

    Remove unused MorphToMany relation import from ParkingMunicipal, ParkingOffstreet, and ParkingSpot models

--- app/Models/ParkingSpot.php
+++ app/Models/ParkingSpot.php
@@ -8,7 +8,6 @@ use App\Traits\Favoritable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ParkingSpot extends Model
