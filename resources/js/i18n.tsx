0bf57699b0d0290e2ca7017f1bb40c2e7599e7a6
Klaas Schoute <klaas_schoute@hotmail.com>
2025-06-20 00:17:11 +0200

    Add sidebar translations for English and Dutch

--- resources/js/i18n.tsx
+++ resources/js/i18n.tsx
@@ -2,15 +2,23 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-import en from '../locales/en/translation.json';
-import nl from '../locales/nl/translation.json';
+import enTranslation from '../locales/en/translation.json';
+import nlTranslation from '../locales/nl/translation.json';
+import enSidebar from '../locales/en/sidebar.json';
+import nlSidebar from '../locales/nl/sidebar.json';
 
 i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources: {
-            en: { translation: en },
-            nl: { translation: nl },
+            en: {
+                translation: enTranslation,
+                sidebar: enSidebar,
+            },
+            nl: {
+                translation: nlTranslation,
+                sidebar: nlSidebar,
+            },
         },
         fallbackLng: 'en',
         supportedLngs: ['en', 'nl'],
