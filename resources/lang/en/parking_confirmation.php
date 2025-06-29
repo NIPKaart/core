fdff733722dab2d96c73c4ad1b318ec041b1f16a
Klaas Schoute <klaas_schoute@hotmail.com>
2025-06-29 22:42:37 +0200

    Add localization for parking confirmation statuses in English and Dutch

---
+++ resources/lang/en/parking_confirmation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'confirmed' => [
+        'label' => 'Confirm availability',
+        'description' => 'The parking space is confirmed to be available.',
+    ],
+    'moved' => [
+        'label' => 'Location moved',
+        'description' => 'The parking space has been moved to a different location.',
+    ],
+    'unavailable' => [
+        'label' => 'Unavailable',
+        'description' => 'The parking space is currently unavailable.',
+    ],
+];
---
+++ resources/lang/nl/parking_confirmation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'confirmed' => [
+        'label' => 'Beschikbaarheid bevestigen',
+        'description' => 'De parkeerplaats is bevestigd als beschikbaar.',
+    ],
+    'moved' => [
+        'label' => 'Locatie verplaatst',
+        'description' => 'De parkeerplaats is verplaatst naar een andere locatie.',
+    ],
+    'unavailable' => [
+        'label' => 'Niet beschikbaar',
+        'description' => 'De parkeerplaats is momenteel niet beschikbaar.',
+    ],
+];
