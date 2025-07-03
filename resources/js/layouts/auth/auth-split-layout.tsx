79ef7db7dd09862c123ef52e674a699c4a292c60
Klaas Schoute <klaas_schoute@hotmail.com>
2025-07-03 23:38:03 +0200

    Add background image for disabled parking layout

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -15,7 +15,8 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" />
+                <div className="absolute inset-0 bg-[url('/assets/images/backgrounds/disabled_parking_2x.webp')] bg-cover bg-center" />
+                <div className="absolute inset-0 bg-black/60" />
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
                     <AppLogo size="lg" />
                 </Link>
