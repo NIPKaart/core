9264ab95388bc01d77f53b7beb0c1a5b60d38ceb
Klaas Schoute <klaas_schoute@hotmail.com>
2025-07-30 14:40:37 +0200

    Use DDEV for local development

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
+// defaults for local DDEV
+const port = 5173;
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
+const corsOrigin = /https?:\/\/([A-Za-z0-9\-.]+)?(\.ddev\.site)(?::\d+)?$/;
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -14,6 +19,18 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    server: {
+        // respond to all network requests
+        host: '0.0.0.0',
+        port: port,
+        strictPort: true,
+        // Defines the origin of the generated asset URLs during development,
+        // this will also be used for the public/hot file (Vite devserver URL)
+        origin: origin,
+        cors: {
+            origin: corsOrigin,
+        },
+    },
     esbuild: {
         jsx: 'automatic',
     },
