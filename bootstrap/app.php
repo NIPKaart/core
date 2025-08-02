797c816d1f3a8cf665323fc96801ea8de8da8d93
Klaas Schoute <klaas_schoute@hotmail.com>
2025-08-03 00:25:04 +0200

    Add trustProxies middleware configuration to application bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'locale', 'sidebar_state']);
 
+        $middleware->trustProxies(at: [
+            '*',
+        ]);
+
         $middleware->web(append: [
             HandleAppearance::class,
             HandleLocale::class,
