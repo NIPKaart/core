74a0d01d9d159ab3574bcb49e1abc653247820bc
Klaas Schoute <klaas_schoute@hotmail.com>
2025-08-31 19:10:20 +0200

    Update project configuration and add laravel/wayfinder dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 // defaults for local DDEV
@@ -18,6 +18,9 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        })
     ],
     server: {
         // respond to all network requests
@@ -33,10 +36,5 @@ export default defineConfig({
     },
     esbuild: {
         jsx: 'automatic',
-    },
-    resolve: {
-        alias: {
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-        },
     },
 });
