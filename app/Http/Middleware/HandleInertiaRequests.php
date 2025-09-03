59fc3d4f13e4d2878b638d91b426aa425ba6745d
Klaas Schoute <klaas_schoute@hotmail.com>
2025-09-04 01:01:01 +0200

    Refactor build process in CI workflow and remove unused Ziggy integration

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Inertia\Middleware;
-use Tighten\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -88,10 +87,6 @@ class HandleInertiaRequests extends Middleware
                         ->toArray(),
                 ]
                 : ['unread' => 0, 'recent' => []],
-            'ziggy' => fn (): array => [
-                ...(new Ziggy)->toArray(),
-                'location' => $request->url(),
-            ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
             'counts' => function () {
                 return [
