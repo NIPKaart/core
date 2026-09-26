cbda4ce289166e96adbc44d313e5cdefef590c72
Klaas Schoute <klaas_schoute@hotmail.com>
2026-09-26 09:08:55 +0100

    Tidy global search map integration

--- resources/js/pages/frontend/map/index.tsx
+++ resources/js/pages/frontend/map/index.tsx
@@ -119,7 +119,6 @@ export default function Map() {
             : null;
     });
 
-
     const [viewportResults, setViewportResults] = useState<ParkingResult[]>([]);
 
     const parkingMarkers = useMemo(
